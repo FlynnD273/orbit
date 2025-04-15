7b7851318857f721b3fafd8e648085b5af13f638
Flynn Duniho <flynn@harveyosity.org>
2025-04-15 16:21:28 -0400

    Support timeline quick view

--- src/c/main.c
+++ src/c/main.c
@@ -16,7 +16,9 @@ static Layer *s_layer;
 static GBitmap *hour_bitmap = NULL;
 static GBitmap *minute_bitmap = NULL;
 static GBitmap *background_bitmap = NULL;
-static GRect window_frame;
+static GRect start_frame;
+static GRect end_frame;
+static GRect curr_frame;
 static int batt_percent;
 static int hour, min;
 
@@ -39,17 +41,25 @@ static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   }
 }
 
-static void background_update_proc(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_frame(layer);
+static void byte_set_bit(uint8_t *byte, uint8_t bit, uint8_t value) {
+  *byte ^= (-value ^ *byte) & (1 << bit);
+}
 
-  // Earth orbit
+static void set_pixel_color(GBitmapDataRowInfo info, GPoint point,
+                            uint8_t color) {
+  uint8_t byte = point.x / 8;
+  uint8_t bit = point.x % 8;
+  byte_set_bit(&info.data[byte], bit, color);
+}
+
+static void background_update_proc(Layer *layer, GContext *ctx) {
   graphics_context_set_stroke_color(
       ctx, PBL_IF_COLOR_ELSE(GColorDarkGray, GColorWhite));
   graphics_context_set_stroke_width(ctx, 2);
   graphics_draw_arc(
       ctx,
-      GRect(bounds.size.w / 2 - earth_orbit_radius,
-            bounds.size.h / 2 - earth_orbit_radius,
+      GRect(curr_frame.size.w / 2 - earth_orbit_radius,
+            curr_frame.size.h / 2 - earth_orbit_radius,
             RAD_TO_DIA(earth_orbit_radius), RAD_TO_DIA(earth_orbit_radius)),
       GOvalScaleModeFitCircle, TRIG_MAX_ANGLE * (100 - batt_percent) / 100,
       TRIG_MAX_ANGLE);
@@ -57,9 +67,9 @@ static void background_update_proc(Layer *layer, GContext *ctx) {
   int angle = TRIG_MAX_ANGLE * hour / 12 + TRIG_MAX_ANGLE * min / 12 / 60 +
               TRIG_MAX_ANGLE * 3 / 4;
   int hour_cx = cos_lookup(angle) * earth_orbit_radius / TRIG_MAX_RATIO +
-                window_frame.size.w / 2;
+                curr_frame.size.w / 2;
   int hour_cy = sin_lookup(angle) * earth_orbit_radius / TRIG_MAX_RATIO +
-                window_frame.size.h / 2;
+                curr_frame.size.h / 2;
 
   angle = TRIG_MAX_ANGLE * min / 60 + TRIG_MAX_ANGLE * 3 / 4;
   int min_cx = cos_lookup(angle) * moon_orbit_radius / TRIG_MAX_RATIO + hour_cx;
@@ -102,15 +112,15 @@ static void background_update_proc(Layer *layer, GContext *ctx) {
   // Dither
   GBitmap *fb = graphics_capture_frame_buffer(ctx);
   // Iterate over all rows
-  for (int y = 0; y < bounds.size.h; y++) {
+  for (int y = 0; y < curr_frame.size.h; y++) {
     // Get this row's range and data
     GBitmapDataRowInfo info = gbitmap_get_data_row_info(fb, y);
-    uint8_t mask = (y % 2) == 0 ? 0x55 : 0xAA;
 
     // Iterate over all visible columns
-    for (int x = info.min_x + (8 - (info.min_x % 8)); x <= info.max_x - 8;
-         x += 8) {
-      info.data[x / 8] &= mask;
+    for (int x = info.min_x; x <= info.max_x; x++) {
+      if ((x + y) % 2) {
+        set_pixel_color(info, GPoint(x, y), 0);
+      }
     }
   }
   graphics_release_frame_buffer(ctx, fb);
@@ -127,17 +137,44 @@ static void background_update_proc(Layer *layer, GContext *ctx) {
                                GRect(min_cx - MOON_RADIUS, min_cy - MOON_RADIUS,
                                      RAD_TO_DIA(MOON_RADIUS),
                                      RAD_TO_DIA(MOON_RADIUS)));
-  graphics_draw_bitmap_in_rect(
-      ctx, background_bitmap,
-      GRect(bounds.size.w / 2 - SUN_RADIUS, bounds.size.h / 2 - SUN_RADIUS,
-            RAD_TO_DIA(SUN_RADIUS), RAD_TO_DIA(SUN_RADIUS)));
+  graphics_draw_bitmap_in_rect(ctx, background_bitmap,
+                               GRect(curr_frame.size.w / 2 - SUN_RADIUS,
+                                     curr_frame.size.h / 2 - SUN_RADIUS,
+                                     RAD_TO_DIA(SUN_RADIUS),
+                                     RAD_TO_DIA(SUN_RADIUS)));
+}
+
+static void handle_area_will_change(GRect final, void *context) {
+  start_frame = GRect(curr_frame.origin.x, curr_frame.origin.y,
+                      curr_frame.size.w, curr_frame.size.h);
+  end_frame = GRect(final.origin.x, final.origin.y, final.size.w, final.size.h);
+}
+
+static void handle_area_change(AnimationProgress progress, void *context) {
+  // We're assuming that the origin remains (0, 0)
+  curr_frame =
+      GRect(0, 0,
+            start_frame.size.w * (ANIMATION_NORMALIZED_MAX - progress) /
+                    ANIMATION_NORMALIZED_MAX +
+                end_frame.size.w * progress / ANIMATION_NORMALIZED_MAX,
+            start_frame.size.h * (ANIMATION_NORMALIZED_MAX - progress) /
+                    ANIMATION_NORMALIZED_MAX +
+                end_frame.size.h * progress / ANIMATION_NORMALIZED_MAX);
+  layer_mark_dirty(s_layer);
+}
+
+static void handle_area_did_change(void *context) {
+  curr_frame = GRect(end_frame.origin.x, end_frame.origin.y, end_frame.size.w,
+                     end_frame.size.h);
+  layer_mark_dirty(s_layer);
 }
 
 static void main_window_load(Window *window) {
   window_layer = window_get_root_layer(window);
-  window_frame = layer_get_frame(window_layer);
-  int min_dim = window_frame.size.w < window_frame.size.h ? window_frame.size.w
-                                                          : window_frame.size.h;
+  GRect window_frame = layer_get_bounds(window_layer);
+  curr_frame = layer_get_unobstructed_bounds(window_layer);
+  int min_dim = curr_frame.size.w < curr_frame.size.h ? curr_frame.size.w
+                                                      : curr_frame.size.h;
   earth_orbit_radius = (min_dim + RAD_TO_DIA(SUN_RADIUS)) / 4;
   moon_orbit_radius = (min_dim + RAD_TO_DIA(EARTH_RADIUS)) / 8;
 
@@ -150,6 +187,9 @@ static void main_window_load(Window *window) {
 
   layer_add_child(window_layer, s_layer);
 
+  // Ensures time is displayed immediately (will break if NULL tick event
+  // accessed). (This is why it's a good idea to have a separate routine to
+  // do the update itself.)
   time_t now = time(NULL);
   struct tm *current_time = localtime(&now);
   handle_minute_tick(current_time, MINUTE_UNIT);
@@ -157,11 +197,15 @@ static void main_window_load(Window *window) {
   handle_battery(battery_state_service_peek());
   tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
   battery_state_service_subscribe(handle_battery);
+  UnobstructedAreaHandlers handlers = {
+      handle_area_will_change, handle_area_change, handle_area_did_change};
+  unobstructed_area_service_subscribe(handlers, NULL);
 }
 
 static void main_window_unload(Window *window) {
   battery_state_service_unsubscribe();
   tick_timer_service_unsubscribe();
+  unobstructed_area_service_unsubscribe();
   layer_destroy(s_layer);
   gbitmap_destroy(background_bitmap);
   gbitmap_destroy(hour_bitmap);
